4eddfed3a8e0ec65b272b33f63492201f1bf9052
Inferno1489 <polikarpich14@yandex.ru>
2018-03-18 12:16:55 +0000

    deb to false

--- CoddyCharity/settings.py
+++ CoddyCharity/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fl%1pn+r3&4zivuse(k(6v-i4b@a+xou16wgoy!f^b@4x_lb!0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ivan1489.pythonanywhere.com']
 
