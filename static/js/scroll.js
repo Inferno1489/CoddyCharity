8aa4586374da6e157654843d0aa64c096b44aa73
Inferno1489 <polikarpich14@yandex.ru>
2018-03-18 13:01:08 +0300

    scroll

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
         event.preventDefault();
         var id  = $(this).attr('href'),
         top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
+        $('body').animate({scrollTop: top}, 1500);
+        $('html').animate({scrollTop: top}, 1500);
     });
 });
