e9cfeda4fd7848669266071ffce1121eadc0806d
Inferno1489 <polikarpich14@yandex.ru>
2018-03-18 13:19:10 +0300

    test 5

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,10 +1,4 @@
-$(".bts").on("click", "a", function(event){
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: $("#"+$(this).attr("pre")).position().top - 120
-        }, 500, 'swing');
-});
-/**$(document).ready(function(){
+$(document).ready(function(){
     $(".bts").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
@@ -12,4 +6,4 @@ $(".bts").on("click", "a", function(event){
         $('body').animate({scrollTop: top}, 1500);
         $('html').animate({scrollTop: top}, 1500);
     });
-});**/
+});
