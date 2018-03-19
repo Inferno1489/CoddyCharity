928aa8ddf22c25ff698719c70df08a708c56f0d5
Inferno1489 <polikarpich14@yandex.ru>
2018-03-20 00:10:51 +0300

    admin-fix222

--- CoddyCharity/settings.py
+++ CoddyCharity/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = "/home/Ivan1489/CoddyCharity/static"
-STATIC_URL = '/CoddyCharity/static/'
+STATIC_URL = '/static/'
