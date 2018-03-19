caa7ced4cd3e3ad95cf26adf8479f07c89e27668
Inferno1489 <polikarpich14@yandex.ru>
2018-03-20 00:00:50 +0300

    bg-fix3

--- CoddyCharity/settings.py
+++ CoddyCharity/settings.py
@@ -120,5 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = "/home/Ivan1489/CoddyCharity/static"
 STATIC_URL = '/static/'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
