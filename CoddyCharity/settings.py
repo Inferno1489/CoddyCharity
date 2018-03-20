9d1128d83e2c71a215eed68279cfddb898e22ca4
Inferno1489 <polikarpich14@yandex.ru>
2018-03-20 08:13:51 +0300

    ura delat fix v 7 1utra

--- CoddyCharity/settings.py
+++ CoddyCharity/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = '/home/Ivan1489/CoddyCharity/static'
 STATIC_URL = '/static/'
-MEDIA_ROOT = '/home/Ivan1489/media'
+MEDIA_ROOT = '/home/Ivan1489/CoddyCharity/media'
 MEDIA_URL = '/media/'
